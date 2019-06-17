0bef2c6fbf3f27350f7f7f945f21949041e3a4c3
berceanu <andreicberceanu@gmail.com>
2019-06-17 15:20:41 +0300

    refactored tests

---
+++ tests/test_util.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `sliceplots` package."""
+
+import numpy as np
+
+from sliceplots import util
+
+
+def test_idx_from_val():
+    """Find element position in array."""
+    position = util.idx_from_val(np.linspace(0, 10, 11), 5.1)
+    assert position == 5
