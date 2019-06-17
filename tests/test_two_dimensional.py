d12ccb462b83f465d917729919464102e85f29ab
berceanu <andreicberceanu@gmail.com>
2019-06-17 23:11:02 +0300

    added todos

--- tests/test_two_dimensional.py
+++ tests/test_two_dimensional.py
@@ -12,8 +12,11 @@ import sliceplots.two_dimensional as two_d
 uu = np.linspace(0, np.pi, 128)
 data = np.cos(uu - 0.5) * np.cos(uu.reshape(-1, 1) - 1.0)
 
+# todo directly use matplotlib's own testing facilities once 3.2.0 is released
+#       see https://matplotlib.org/3.1.0/devel/testing.html
 
 @pytest.mark.mpl_image_compare()
+# https://github.com/matplotlib/pytest-mpl
 def test_plot2d():
     """Checks ``Plot2D`` class."""
     p2d = two_d.Plot2D(data,
