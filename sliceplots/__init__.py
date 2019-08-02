298553b3a2da595a968930fdf64436dd0d643e55
Andrei Berceanu <andreicberceanu@gmail.com>
2019-08-02 22:36:47 +0300

    Bump version: 0.2.0 → 0.2.1

--- sliceplots/__init__.py
+++ sliceplots/__init__.py
@@ -16,4 +16,4 @@ from sliceplots.two_dimensional import Plot2D  # NOQA: F401
 
 __author__ = """Andrei Berceanu"""
 __email__ = "andreicberceanu@gmail.com"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
